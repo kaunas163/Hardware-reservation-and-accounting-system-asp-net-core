a7b131d0e40f9cbed82c6d34f867818b9dec7b36
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2019-04-30 21:45:29 +0300

    Created more controllers and views

---
+++ Controllers/ReservationsController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using HardwareReservationAndAccountingSystem.Data;
+
+namespace HardwareReservationAndAccountingSystem.Controllers
+{
+    public class ReservationsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReservationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
