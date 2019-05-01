9629f198cb22041a57c3b65b866cc5280f52d7d7
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2019-05-01 17:56:35 +0300

    Compile ES6 code, add FullCalendar dependency

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,16 @@
 const gulp      = require('gulp');
 const sass      = require('gulp-sass');
-const concat    = require('gulp-concat');
+// const concat    = require('gulp-concat');
 const concatCss = require('gulp-concat-css');
 const cleanCss  = require('gulp-clean-css');
 const uglify    = require('gulp-uglify');
 const rename    = require("gulp-rename");
+const babel     = require('gulp-babel');
+const browserify = require('browserify');
+const source    = require('vinyl-source-stream');
+const buffer    = require('vinyl-buffer');
+const del       = require('del');
+const fs        = require('fs-extra');
 
 function fonts(done) {
     gulp.src('./node_modules/@fortawesome/fontawesome-free/webfonts/*')
@@ -32,19 +38,34 @@ function styles(done) {
     done();
 }
 
-function scripts(done) {
+function cleanTemp(done) {
+    del(['./wwwroot/temp/*']);
+    done();
+}
+
+function es6common(done) {
+    fs.ensureDirSync('./wwwroot/temp');
     gulp.src([
+        './Scripts/*.js',
+        '/Scripts/**/*.js',
+    ])
+    .pipe(babel())
+    .pipe(gulp.dest('./wwwroot/temp/'));
+    done();
+}
+
+function scripts(done) {
+    browserify([
         './node_modules/jquery/dist/jquery.slim.js',
         './node_modules/jquery-validation/dist/jquery.validate.js', // TODO: include localization of this
         './node_modules/popper.js/dist/umd/popper.js',
         './node_modules/bootstrap/dist/js/bootstrap.js',
-        './node_modules/vue/dist/vue.js',
-        // './node_modules/@fullcalendar/core/main.js',
-        // './node_modules/@fullcalendar/daygrid/main.js',
         './node_modules/lightbox2/src/js/lightbox.js',
-        './Scripts/index.js',
+        './wwwroot/temp/index.js',
     ])
-    .pipe(concat('scripts.js'))
+    .bundle()
+    .pipe(source('scripts.js'))
+    .pipe(buffer())
     .pipe(uglify())
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('./wwwroot/js/'));
@@ -57,4 +78,11 @@ function watch(done) {
     done();
 }
 
-exports.default = gulp.series(gulp.parallel(fonts, styles, scripts), watch);
+exports.fonts = fonts;
+exports.styles = styles;
+exports.es6common = es6common;
+exports.scripts = scripts;
+exports.watch = watch;
+exports.cleanTemp = cleanTemp;
+
+exports.default = gulp.series(es6common, gulp.parallel(fonts, styles, scripts), gulp.parallel(cleanTemp, watch));
---
+++ Scripts/index.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import Calendar from './Calendar.js'
+
+new Vue({
+    render: h => h(Calendar)
+}).$mount('#demo-app-placeholder')
+
