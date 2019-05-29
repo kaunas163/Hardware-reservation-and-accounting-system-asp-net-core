45082dce9229aad52b92787e2c745d448948e9a0
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2019-05-29 14:02:05 +0300

    Print only current user notifications in notifications view

--- Controllers/NotificationsController.cs
+++ Controllers/NotificationsController.cs
@@ -5,6 +5,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using HardwareReservationAndAccountingSystem.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using HardwareReservationAndAccountingSystem.Models;
+using System.Threading.Tasks;
 
 namespace HardwareReservationAndAccountingSystem.Controllers
 {
@@ -12,16 +15,24 @@ namespace HardwareReservationAndAccountingSystem.Controllers
     public class NotificationsController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public NotificationsController(ApplicationDbContext context)
+        public NotificationsController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var model = _context.Notifications.Include(x => x.NotificationType).ToList();
-            return View(model);
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+
+            var notifications = _context.Notifications
+                .Include(x => x.NotificationType)
+                .Where(x => x.NotificationsForUsers.Any(u => u.User.Id == user.Id))
+                .ToList();
+
+            return View(notifications);
         }
 
         public IActionResult Details(int id)
