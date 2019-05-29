bd3191789f4dc6a67c75d1f0e6fed235498ec4b8
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2019-05-30 02:12:46 +0300

    Print reservations

--- Controllers/ReservationsController.cs
+++ Controllers/ReservationsController.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using HardwareReservationAndAccountingSystem.Data;
+using Microsoft.AspNetCore.Identity;
+using HardwareReservationAndAccountingSystem.Models;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace HardwareReservationAndAccountingSystem.Controllers
 {
@@ -11,15 +15,31 @@ namespace HardwareReservationAndAccountingSystem.Controllers
     public class ReservationsController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public ReservationsController(ApplicationDbContext context)
+        public ReservationsController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
         {
-            return View();
+            //var user = await _userManager.GetUserAsync(HttpContext.User);
+
+            //var notifications = _context.Notifications
+            //    .Include(x => x.NotificationType)
+            //    .Include(x => x.NotificationsForUsers).ThenInclude(u => u.User)
+            //    .Where(x => x.NotificationsForUsers.Any(u => u.User.Id == user.Id))
+            //    .ToList();
+
+            var reservations = _context.Reservations
+                .Include(x => x.Customer)
+                .Include(x => x.EquipmentBundle)
+                .Include(x => x.Event)
+                .ToList();
+
+            return View(reservations);
         }
 
         public IActionResult Details()
