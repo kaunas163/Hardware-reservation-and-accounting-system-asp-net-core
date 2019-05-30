f45fe6c8a1170e4179dcab9a9d2c1ebc4eacf994
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2019-05-30 03:43:15 +0300

    Attemp to fix incorrect redirects in browser

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -43,15 +43,15 @@ namespace HardwareReservationAndAccountingSystem.Controllers
             return View(model);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
     }
 }
