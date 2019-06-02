38df8c6eff3910c5077961d3c75a6eb04f875b9b
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2019-06-02 18:53:27 +0300

    Show right modals for editing reservation status

---
+++ ViewsModels/Reservations/ReservationStatusEdit.cs
@@ -0,0 +1,16 @@
+﻿using HardwareReservationAndAccountingSystem.Enums;
+using HardwareReservationAndAccountingSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HardwareReservationAndAccountingSystem.ViewsModels.Reservations
+{
+    public class ReservationStatusEdit
+    {
+        public ReservationStatus ReservationStatus { get; set; }
+
+        public Reservation Reservation { get; set; }
+    }
+}
